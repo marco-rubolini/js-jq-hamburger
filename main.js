68a00fed22122b832fbb341cf5c157356aa6ce40
Marco <rubolini.marco@gmail.com>
2020-04-29 17:04:08 +0200

    Ottimizzo main.js e inserisco nei commenti altri metodi

--- main.js
+++ main.js
@@ -1,17 +1,38 @@
-// $('.fa-bars').click(
+//Visualizzo il menu hamburger con show e hide
+
+// $('.header-right a').click(
 //     function () {
 //         $('.hamburger-menu').show();
 //     }
 // );
 //
-// $('.fa-times').click(
+// $('.hamburger-menu a').click(
 //     function () {
 //         $('.hamburger-menu').hide();
 //     }
 // );
 
-$('.hamburger-menu a, .header-right a').click(
+//Visualizzo il menu hamburger su mobile utilizzando toggle
+//
+// $('.hamburger-menu a, .header-right a').click(
+//     function () {
+//         $('.hamburger-menu').toggle();
+//     }
+// );
+
+
+// // Visualizzo il menu hamburger su mobile aggiungendo la classe .active già presente nel css
+
+$(".header-right a").click(
     function () {
-        $('.hamburger-menu').toggle();
+        $('.hamburger-menu').addClass("active");
+    }
+)
+
+// // Nascondo il menu hamburger rimuovendo la classe .active al click dell'utente su close
+
+$('.hamburger-menu a').click(
+    function () {
+        $('.hamburger-menu').removeClass("active");
     }
 );
